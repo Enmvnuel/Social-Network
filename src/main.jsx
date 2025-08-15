17d87b08cdf2c4f3c99d0baa07fdc64549419ea4
enmvnuel <cristopherariasp@icloud.com>
2025-08-14 21:43:43 -0500

    Add Footer and Information components; update main.jsx to include them

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Hero from "./Hero.jsx";
 import Logos from "./Logos.jsx";
+import Information from "./Information.jsx";
+import Footer from "./Footer.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <Hero />
         <Logos />
+        <Information />
+        <Footer />
     </StrictMode>
 );
