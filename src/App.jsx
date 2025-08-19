b5f2b6b04d72048fba4d7be7db0b4c50f146241e
ysEnmanuel <cristopherariasp@icloud.com>
2025-08-18 19:44:01 -0500

    Refactor main entry point to render App component; consolidate component imports into App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Hero from "./Hero.jsx";
+import Logos from "./Logos.jsx";
+import FeedEventos from "./FeedEventos.jsx";
+import Testimonial from "./Testimonial.jsx";
+import JoinWailtlist from "./JoinWaitlist.jsx";
+import Footer from "./Footer.jsx";
+
+function App() {
+    return (
+        <>
+            <Hero />
+            <Logos />
+            <FeedEventos />
+            <Testimonial />
+            <JoinWailtlist />
+            <Footer />
+        </>
+    );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Hero from "./Hero.jsx";
-import Logos from "./Logos.jsx";
-import FeedEventos from "./FeedEventos.jsx";
-import Testimonial from "./Testimonial.jsx";
-import JoinWailtlist from "./JoinWaitlist.jsx";
-import Footer from "./Footer.jsx";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <Hero />
-        <Logos />
-        <FeedEventos />
-        <Testimonial />
-        <JoinWailtlist />
-        <Footer />
+        <App />
     </StrictMode>
 );
