6dc25f4c6c1c11abb60c892e5bf36807ac9fc12d
enmvnuel <cristopherariasp@icloud.com>
2025-08-22 23:57:47 -0500

    Adds blurred background to authentication pages
    Enhances the visual appeal of the sign-in and sign-up pages by incorporating a blurred background image. This provides a more consistent and engaging look for the authentication flow.

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -3,7 +3,14 @@ import { SignIn } from "@clerk/clerk-react";
 export default function SignInPage() {
     return (
         <>
-            <div className="flex justify-center items-center min-h-screen">
+            <div className="relative flex justify-center items-center min-h-screen">
+                {/* Fondo desenfocado */}
+                <div
+                    className="absolute inset-0 bg-[url('../public/assets/img/bg.png')] bg-cover blur-2xl -z-10 "
+                    aria-hidden="true"
+                />
+
+                {/* Contenido sobre fondo */}
                 <SignIn
                     afterSignInUrl="/dashboard"
                     afterSignUpUrl="/dashboard"
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -3,7 +3,12 @@ import { SignUp } from "@clerk/clerk-react";
 
 export default function SignUpPage() {
     return (
-        <div className="flex justify-center items-center min-h-screen">
+        <div className="relative flex justify-center items-center min-h-screen">
+            <div
+                className="absolute inset-0 bg-[url('../public/assets/img/bg.png')] bg-cover blur-2xl -z-10"
+                aria-hidden="true"
+            />
+
             <SignUp
                 afterSignUpUrl="/dashboard"
                 afterSignInUrl="/dashboard"
