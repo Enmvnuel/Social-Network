ad82e460ecd6e0070370b4b30b34365182959a98
enmvnuel <cristopherariasp@icloud.com>
2025-08-23 00:06:13 -0500

    Fix formatting in SignInPage and SignUpPage components

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -6,7 +6,7 @@ export default function SignInPage() {
             <div className="relative flex justify-center items-center min-h-screen">
                 {/* Fondo desenfocado */}
                 <div
-                    className="absolute inset-0 bg-[url('../public/assets/img/bg.png')] bg-cover blur-2xl -z-10 "
+                    className="absolute inset-0 bg-[url('../assets/img/bg.png')] bg-cover blur-2xl -z-10 "
                     aria-hidden="true"
                 />
 
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -5,7 +5,7 @@ export default function SignUpPage() {
     return (
         <div className="relative flex justify-center items-center min-h-screen">
             <div
-                className="absolute inset-0 bg-[url('../public/assets/img/bg.png')] bg-cover blur-2xl -z-10"
+                className="absolute inset-0 bg-[url('../assets/img/bg.png')] bg-cover blur-2xl -z-10"
                 aria-hidden="true"
             />
 
